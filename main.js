347dee748e56f934403d81b01711fe09ef333bb3
Danielle Higgins <higdan4024@gmail.com>
2024-10-27 17:53:14 -0400

    added copy to clipboard feature

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ const person = document.getElementById("person");
 
 const newQuoteBtn = document.getElementById("new-quote");
 const speechBtn = document.getElementById("speech");
+const copyBtn = document.getElementById("copy");
 
 newQuoteBtn.addEventListener("click", () => {
   // Found an API that doesnt give me any CORS issues 😃
   fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // console.log(data);
       // console.log(data[0]);
 
       quote.textContent = data[0].quote;
@@ -26,4 +27,27 @@ speechBtn.addEventListener("click", () => {
 
   const utterPerson = new SpeechSynthesisUtterance(person.textContent);
   synth.speak(utterPerson);
+});
+
+copyBtn.addEventListener("click", () => {
+  // It is important to know that the Clipboard API is only supported for pages served over HTTPS.
+
+  // You should check for browser permissions
+  navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
+    if (result.state === "granted" || result.state === "prompt") {
+      console.log("Write access granted!");
+    }
+  });
+
+  // To copy text with the new Clipboard API, you will use the asynchronous writeText() method
+  navigator.clipboard
+    .writeText(`${quote.textContent} By ${person.textContent}`)
+    .then(
+      () => {
+        console.log("Content copied to clipboard");
+      },
+      () => {
+        console.error("Failed to copy");
+      }
+    );
 });
